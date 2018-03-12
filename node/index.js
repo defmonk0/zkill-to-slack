eec2cf3bd268ef09861b43e3043896a229aaf960
Steve Harrigan <def.monk0@gmail.com>
2018-03-12 12:52:47 -0400

    Changed formatting for personal use.

--- node/index.js
+++ node/index.js
@@ -1,16 +1,18 @@
-var monitorZkill = require('../src/monitorZkill');
-var setEnvironmentVariables = require('./config/setEnvironmentVariables');
+var monitorZkill = require("../src/monitorZkill");
+var setEnvironmentVariables = require("./config/setEnvironmentVariables");
 
 setEnvironmentVariables();
 
-console.log('queueID: ' + process.env.queueID);
-console.log('slackHookURL: ' + process.env.slackHookURL);
-console.log('channel: ' + process.env.channel);
-console.log('watchForCorp: ' + process.env.watchForCorp);
-console.log('watchForAlliance: ' + process.env.watchForAlliance);
+console.log("queueID: " + process.env.queueID);
+console.log("slackHookURL: " + process.env.slackHookURL);
+console.log("channel: " + process.env.channel);
+console.log("watchForCorp: " + process.env.watchForCorp);
+console.log("watchForAlliance: " + process.env.watchForAlliance);
 
-function watch () {
- setTimeout(function() { monitorZkill(watch) }, 10000);
-};
+function watch() {
+	setTimeout(function() {
+		monitorZkill(watch);
+	}, 10000);
+}
 
 watch();
